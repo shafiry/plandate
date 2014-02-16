9dcca836180ff9c291a87676799b3b480fb6b49f
shafiry <yuvalshafir@yahoo.com>
2014-02-16 08:30:59 -0500

    globals

--- plandate.py
+++ plandate.py
@@ -23,16 +23,42 @@ def respond():
     if "done" in body.lower():
         resp.sms("Hope you enjoyed your date! Thank you for using PlanDate!")
     else:
-	print_info()
+	if activityLevel == 3:
+            url_params['category_filter'] = 'active'
+        elif activityLevel == 2:
+            activities = ['bars','nightlife','arts','restaurants','fashion','artsandcrafts','zoos','skatingrinks','beaches']
+            url_params['category_filter'] = activities[random.randint(0,len(activities)-1)]
+        else:
+            activities = ['arts','restaurants','artsandcrafts','fashion']
+            url_params['category_filter'] = activities[random.randint(0,len(activities)-1)]
+    
+        url_params['location'] = add
+        url_params['radius_filter'] = radius
+        """sends the text given 'phone'"""
+        response = sendText.requester('api.yelp.com', '/v2/search', url_params, '0px8qDLNaYLpwVHlGeyNzQ', 'VYdp1bHYoPayFF9Mp0Q4RP328n0','RoXmcii1RiXkAoFh1uwMZpC6_5k2iVyq', '0bbsAocDgcBsua5WvOzxU6NxGUY')
+        venueName = ''
+        venueLocation = []
+        for key, value in response["businesses"][random.randint(0,4)].iteritems():
+            if key == 'name':
+                venueName = value
+            if key == 'location':
+                for i in range(0,len(value['display_address'])):
+                    venueLocation.append(value['display_address'][i])
+        venue = ''
+        account_sid = "ACfe656ed49f19a12b8440cb191158f0c9"
+        auth_token = "8d29d5cc81e4062e1521237983c39b21"
+        client = TwilioRestClient(account_sid, auth_token)    
+	resp.sms("Next Up:\n" + venueName +"\n"+ "\n".join(venueLocation) + "\n Reply 'next' for next plan, 'done' if finished.")
+	
     return str(resp)
 
 @app.route('/_print_info')
 def print_info():
     """returns the data"""
-    phone = request.args.get('phone_id')
-    add = request.args.get('address')
-    radius = request.args.get('distance')
-    activityLevel = request.args.get('activity')
+    global phone = request.args.get('phone_id')
+    global add = request.args.get('address')
+    global radius = request.args.get('distance')
+    global activityLevel = request.args.get('activity')
 
     if activityLevel == 3:
         url_params['category_filter'] = 'active'
