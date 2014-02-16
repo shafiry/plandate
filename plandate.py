73d16cf7113c02f8dd3f6e9acfbdb9d119875baa
shafiry <yuvalshafir@yahoo.com>
2014-02-15 20:28:48 -0500

    word

--- plandate.py
+++ plandate.py
@@ -13,6 +13,29 @@ url_params['sort'] = 0
 
 app = Flask(__name__, static_url_path = "", static_folder = "static")
 
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+    "+15162387865": "Lucas"
+}
+
+@app.route("/",methods=['GET','POST'])
+def hello_monkey():
+    """Respond and greet the caller by name."""
+    
+    from_number = request.values.get('From', None)
+    if from_numer in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+    
+    resp = twilio.twiml.Response()
+    resp.message(message)
+
+    return str(resp)
+    
+
 @app.route('/_print_info')
 def print_info():
     """returns the data"""
@@ -56,28 +79,6 @@ def index():
     return render_template('index.html')
 
 
-callers = {
-    "+14158675309": "Curious George",
-    "+14158675310": "Boots",
-    "+14158675311": "Virgil",
-    "+15162387865": "Lucas"
-}
-
-@app.route("/",methods=['GET','POST'])
-def hello_monkey():
-    """Respond and greet the caller by name."""
-    
-    from_number = request.values.get('From', None)
-    if from_numer in callers:
-        message = callers[from_number] + ", thanks for the message!"
-    else:
-        message = "Monkey, thanks for the message!"
-    
-    resp = twilio.twiml.Response()
-    resp.message(message)
-
-    return str(resp)
-    
 
 if __name__ == '__main__':
     app.run()
