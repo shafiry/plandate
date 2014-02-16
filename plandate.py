e90617a15709a8c943fa7795972ccd99cae1a126
shafiry <yuvalshafir@yahoo.com>
2014-02-15 20:21:06 -0500

    recieving

--- plandate.py
+++ plandate.py
@@ -56,13 +56,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/", methods=['GET', 'POST'])
+callers = {
+    "+14158675309": "Curious George",
+    "+14158675310": "Boots",
+    "+14158675311": "Virgil",
+    "+15162387865": "Lucas"
+}
+
+@app.route("/",methods=['GET','POST'])
 def hello_monkey():
-    """Respond to incoming calls with a simple text message."""
- 
+    """Respond and greet the caller by name."""
+    
+    from_number = request.values.get('From', None)
+    if from_numer in callers:
+        message = callers[from_number] + ", thanks for the message!"
+    else:
+        message = "Monkey, thanks for the message!"
+    
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message(message)
+
     return str(resp)
+    
 
 if __name__ == '__main__':
     app.run()
